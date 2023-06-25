cc51460ed42223732352fd69ff3bddd17feb8375
Nisarg <kavinisarg1@gmail.com>
2023-06-25 19:26:04 +0530

    Init Push 💪

---
+++ README.md
@@ -0,0 +1,13 @@
+# EMOSVG
+Use emojis as svgs in your project throw a link!
+
+example - 
+```html
+...
+<head>
+    <link rel="icon" href="https://emosvg.nisargkavi.in/😂"/> 
+</head>
+...
+```
+
+Effortlessly Convert Emojis to SVG Favicons for Your Project!
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <pre>example request: <a href="javascript:void(0)">https://emosvg.nisargkavi.in/❤️</a></pre>
+      <pre>example: <br />`&lt;link rel="icon" href="https://emosvg.nisargkavi.in/❤️" /&gt;`</pre>
+    </div>
+  )
+}
