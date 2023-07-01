8153b8962880d7c64547acf8d9777dd9fc36b834
Nisarg <kavinisarg1@gmail.com>
2023-07-01 15:36:49 +0530

    Major Improvement 🙌

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:emoji*",
+        destination: "/api/emoji?emoji=:emoji*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/api/emoji/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { parse } from "twemoji-parser";
+
+export async function GET(req, res) {
+  try {
+    const url = new URL(req.url, `${req.protocol}://${req.headers.host}`);
+    console.log(url.pathname);
+    const emoji = url.searchParams.get("emoji");
+    const parsedEmoji = parse(emoji ? emoji : decodeURIComponent(url.pathname));
+
+    if (parsedEmoji.length === 0) throw new Error('Emoji not found!');
+
+    const response = await fetch(parsedEmoji[0].url);
+    const buffer = Buffer.from(await response.arrayBuffer());
+
+    return new NextResponse(buffer, {
+      headers: {
+        "Content-Type": response.headers.get("Content-Type"),
+      },
+      status: 200,
+    });
+  } catch (e) {
+    return new NextResponse(
+      { message: "Server error, please try again!" },
+      { status: 500 }
+    );
+  }
+}
